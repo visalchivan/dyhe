f2748696ab429d300801ec6f05c2e69ae2c82f9e
Visal Infinity <visal211.infinity@gmail.com>
2025-10-29 08:29:15 +0700

    back to old one

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,23 +1,4 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { nextJsConfig } from "@repo/eslint-config/next-js";
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-])
+/** @type {import("eslint").Linter.Config} */
+export default nextJsConfig;
